9fa68de0703ac8267cca0f313ecb3f315fc6a925
1DPS <3254957253@qq.com>
2025-11-26 15:14:18 +0800

    feat: initial commit of eBusiness backend

---
+++ src/main/java/com/ch/ebusiness/controller/admin/AdminBaseController.java
@@ -0,0 +1,22 @@
+package com.ch.ebusiness.controller.admin;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import com.ch.ebusiness.NoLoginException;
+
+@Controller
+public class AdminBaseController {
+    /**
+     * 登录权限控制，处理方法执行前执行该方法
+     */
+    @ModelAttribute
+    public String isLogin(HttpSession session) throws NoLoginException {
+
+        if (session.getAttribute("auser") == null) {
+            return "redirect:/admin/toLogin";
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/ch/ebusiness/repository/before/UserRepository.java
@@ -0,0 +1,16 @@
+package com.ch.ebusiness.repository.before;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ch.ebusiness.entity.BUser;
+
+@Mapper
+public interface UserRepository {
+    public List<BUser> isUse(BUser bUser);
+
+    public int register(BUser bUser);
+
+    public List<BUser> login(BUser bUser);
+}
